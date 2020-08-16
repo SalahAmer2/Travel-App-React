d0957e3daa99701c899679d118a516d3121db7b5
SalahAmer2 <salahayman2@gmail.com>
2020-08-16 19:36:00 +0200

    removed payload from toggles in pop-up.actions.js, because you shouldn't use payload if you're just toggling a boolean

--- src/redux/pop-up/pop-up.actions.js
+++ src/redux/pop-up/pop-up.actions.js
@@ -1,22 +1,19 @@
 import { PopUpActionTypes } from "./pop-up.types";
 
 export const toggleShowPopUp = popUp => ({
-    type: PopUpActionTypes.TOGGLE_SHOW_POP_UP,
-    payload: popUp
+    type: PopUpActionTypes.TOGGLE_SHOW_POP_UP
 })
 
-export const togglePopUpExit = popUp => ({// See if you need to use payload in the first place to toggle boolean
-    type: PopUpActionTypes.TOGGLE_POP_UP_EXIT,           // Now we'll see the reason and way to use reducers other than the root-reducer
-    //payload: popUp                      // Because you could simply just ! the boolean here in the payload, but we're not
-                                          // doing that because we're using the pop-up.reducer.js
+export const togglePopUpExit = popUp => ({// See if you need to use payload in the first place to toggle boolean// Answer: YOU DON'T USE PAYLOAD TO TOGGLE BOOLEAN
+    type: PopUpActionTypes.TOGGLE_POP_UP_EXIT           // Now we'll see the reason and way to use reducers other than the root-reducer 
+                                               // Because you could simply just ! the boolean here in the payload, but we're not
+                                               // doing that because we're using the pop-up.reducer.js
 })
 
 export const togglePopUp3State = popUp => ({
-    type: PopUpActionTypes.TOGGLE_POP_UP_3_STATE,
-    payload: popUp
+    type: PopUpActionTypes.TOGGLE_POP_UP_3_STATE
 })
 
 export const toggleSubmittedOrNot = popUp => ({
-    type: PopUpActionTypes.TOGGLE_SUBMITTED_OR_NOT,
-    payload: popUp
+    type: PopUpActionTypes.TOGGLE_SUBMITTED_OR_NOT
 })
