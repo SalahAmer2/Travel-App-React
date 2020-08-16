a8a0773fd1a253fa5537f398e0b388a5f1dd5174
SalahAmer2 <salahayman2@gmail.com>
2020-08-16 19:51:14 +0200

    put togglePopUpExit in the onClick instead of handleExit

--- src/components/all-pop-ups/pop-up-2/pop-up-2.component.jsx
+++ src/components/all-pop-ups/pop-up-2/pop-up-2.component.jsx
@@ -2,24 +2,15 @@ import React from "react";
 import { connect } from "react-redux";
 import { togglePopUpExit } from "../../../redux/pop-up/pop-up.actions";
 
-class Pop_Up_2 extends Component {
+const Pop_Up_2 = ({ togglePopUpExit, pop_up_exit }) => (
+    <div id="pop-up-2" className={`pop-up pop-up-drop ${pop_up_exit ? "pop-up-swing" : ""}`}>
+        <div className="container">
+            <div className="exit" onClick={togglePopUpExit}>x</div>
+            <h1>Empty city name / dates</h1>
+        </div>
+    </div>
+)
 
-    handleExit = () => {
-        this.props.togglePopUpExit()
-    }
-
-    render ({ handleExit, pop_up_exit }) {     
-        return (
-            <div id="pop-up-2" className={`pop-up pop-up-drop ${pop_up_exit ? "pop-up-swing" : ""}`}>
-                <div className="container">
-                    <div className="exit" onClick={handleExit}>x</div>
-                    <h1>Empty city name / dates</h1>
-                </div>
-            </div>
-        )
-    }
-
-}
 const mapStateToProps = state => ({
     pop_up_exit: state.popUp.pop_up_exit
 });
